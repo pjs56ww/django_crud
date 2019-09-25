15b3b26bb1ce9679290e76e0defd96761acd1d9d
pjs56ww <sysh5656@naver.com>
2019-09-25 16:18:46 +0900

    Job 에서 media 파일 실습

--- jobs/models.py
+++ jobs/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Job(models.Model):
     Name = models.CharField(max_length=20)
     past_job = models.TextField()
+    profile_image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.name
--- jobs/views.py
+++ jobs/views.py
@@ -4,7 +4,6 @@ from faker import Faker
 fake = Faker()
 
 def insert(request):
-
     return render(request, 'jobs/insert.html')
 
 # GIF_API = f'https://api.giphy.com/v1/gifs/search?api_key=yffLcxywQ73zcI0YssOgH5IvPpi9qCU1&q={ p_j }&limit=1&offset=0&rating=G&lang=en'
@@ -13,11 +12,14 @@ def insert(request):
 def past_job(request):
     jobs = Job.objects.all()
     name = request.POST.get('Name')
+    image = request.FILES.get('image')
     if jobs.filter(Name=name):
+
         job = Job.objects.get(Name=name)
     else:
         pastjob = fake.job()
         job = Job()
+        job.profile_image = image
         job.Name = name
         job.past_job = pastjob
         job.save()
